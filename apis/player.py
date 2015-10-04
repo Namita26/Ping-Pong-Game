1b6e12aecf5301d3e8af46b07e5604a96f2b538f
Namita Maharanwar <namitamaharanwar@gmail.com>
2015-10-04 21:24:16 +0530

    player registraion

--- apis/player.py
+++ apis/player.py
@@ -5,6 +5,9 @@ Date: 4 Oct, 2015
 
 from flask import Flask
 from flask_restful import Resource, Api
+# from apis.referee import Championship
+import requests
+import json
 
 app = Flask(__name__)
 api = Api(app)
@@ -17,7 +20,7 @@ class Player(Resource):
     def __init__(self, id, name, defence_length):
         self.player_id = id
         self.player_name = name
-        self.defence_set_lenth = defence_length
+        self.defence_set_length = defence_length
 
     def is_shut_down(self, id, score):
         if score != 5:
@@ -27,4 +30,8 @@ api.add_resource(Player, '/')
 
 
 if __name__ == "__main__":
+    with open("apis/players_info.json", "r") as f:
+        players_info = json.load(f)
+    payload = players_info[2]
+    print requests.post("http://127.0.0.1:5001/register", data=payload)
     app.run(debug=True)
