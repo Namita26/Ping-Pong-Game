57b68394dd795e40819907441bccc96a09d813d4
Namita Maharanwar <namitamaharanwar@gmail.com>
2015-10-05 00:41:25 +0530

    all player particpating

--- apis/player.py
+++ apis/player.py
@@ -32,6 +32,6 @@ api.add_resource(Player, '/')
 if __name__ == "__main__":
     with open("apis/players_info.json", "r") as f:
         players_info = json.load(f)
-    payload = players_info[3]
-    print requests.post("http://127.0.0.1:5001/register", data=payload)
+    for payload in players_info:
+        print requests.post("http://127.0.0.1:5001/register", data=payload)
     app.run(debug=True)
