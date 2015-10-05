e0f10a400fdc983bb8d7f8dd594da6c4ebb5f4a0
Namita Maharanwar <namitamaharanwar@gmail.com>
2015-10-05 23:34:55 +0530

    commence method and docstrings

--- apis/referee.py
+++ apis/referee.py
@@ -11,26 +11,73 @@ app = Flask(__name__)
 api = Api(app)
 
 joined_players = {}
-
-
-class Referee(Resource):
-    def get(self):
-        return "Hiii"
+winner_ids = []
 
 
 parser = reqparse.RequestParser()
 
 
 class Registration(Resource):
+    """
+    Player Registration
+    """
+
+    def __init__(self):
+        self.matches_between = [('1', '2'), ('3', '4'), ('5', '6'), ('7', '8')]
 
     def post(self):
-        id = request.form['id']
-        joined_players[id] = {}
-        joined_players[id]['player_name'] = request.form['name']
-        joined_players[id]['defence_set_length'] = request.form['length']
-        joined_players[id]['is_alive'] = 1
-        if len(joined_players) >= 8:
-            Match().start_game()
+        """
+        POST method for player registration in tournament
+        sample payload = {
+            '1': {
+                'is_alive': 1,
+                'player_name': u'Joey',
+                'defence_set_length': u'8'
+            }
+        }
+        """
+        p_id = request.form['id']
+        joined_players[p_id] = {}
+        joined_players[p_id]['player_name'] = request.form['name']
+        joined_players[p_id]['defence_set_length'] = request.form['length']
+        joined_players[p_id]['is_alive'] = 1
+        if len(joined_players) == 8:
+            self.commence()
+
+    def commence(self):
+        """
+        Start the game between players
+        """
+        round_winners = []
+
+        if len(self.matches_between) == 0:
+            return
+
+        for i in xrange(0, len(self.matches_between)):
+            if len(self.matches_between) != 0:
+                players_tuple = self.matches_between.pop()
+
+                player1_id = players_tuple[0]
+                player2_id = players_tuple[1]
+
+                player1 = Match._generate_random_array(
+                    joined_players[player1_id]['defence_set_length'])
+                player2 = Match._generate_random_array(
+                    joined_players[player2_id]['defence_set_length'])
+                round_winners.append(
+                    Match().game(player1, player2, player1_id, player2_id))
+        print winner_ids
+        next_matches = []
+
+        if len(round_winners) == 1:
+            print "The Winner is ", round_winners[0]
+            return
+
+        for j in xrange(0, len(round_winners), 2):
+            next_matches.append((round_winners[j], round_winners[j+1]))
+            self.matches_between = next_matches
+
+        self.commence()
 
     def get(self):
         return joined_players
@@ -38,18 +85,18 @@ class Registration(Resource):
 
 MAIN_ARRAY = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
+
 class Match():
-    def __init__(self):
-        self.matches_between = [('1', '2'), ('3', '4'), ('5', '6'), ('7', '8')]
 
-    def start_game(self):
-        player1 = Match._generate_random_array(
-            joined_players[self.matches_between[0][0]]['defence_set_length'])
-        player2 = Match._generate_random_array(
-            joined_players[self.matches_between[0][1]]['defence_set_length'])
-        Match().game(player1, player2)
-
-    def game(self, player1, player2):
+    def game(self, player1, player2, player1_id, player2_id):
+        """
+        Game between two players
+        :param player1 is the defence set array of player1
+        :param player2 is the defence set array of player2
+        :param player1_id is the id of player1
+        :param player2_id is the id of player2
+        :return ID of the winner
+        """
         offensive = "player1"
         player1_score = 0
         player2_score = 0
@@ -69,16 +116,20 @@ class Match():
                     player2_score = player2_score + 1
 
         if player1_score > player2_score:
-            print "Player1", player1_score
+            return player1_id
         else:
-            print "Player2", player2_score
+            return player2_id
 
     @staticmethod
     def _generate_random_array(length):
+        """
+        Generates array of input length containing random numbers ranged between
+        1-10
+        :param length of the defence set array
+        """
         return sample(MAIN_ARRAY, int(length))
 
 
-api.add_resource(Referee, '/')
 api.add_resource(Registration, '/register')
 
 
